e70b2d659ed44bc4ff3d5ab1e6596401ba5253d3
beho <sperka@gmail.com>
2014-05-08 23:17:46 +0200

    capistrano config

---
+++ Procfile
@@ -0,0 +1,3 @@
+api: ./bin/api_server.sh
+tx_process: ./bin/tx_processor.sh
+tx_server: ./bin/tx_server.sh
--- bin/tx_processor.sh
+++ bin/tx_processor.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
 ruby config/boot_tx_processor.rb
-# sneakers work TxProcessor --require config/boot.rb
