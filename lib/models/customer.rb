560440231089d694661a8c52fc17d8101d70aa07
beho <sperka@gmail.com>
2014-05-14 20:53:55 +0200

    fix balance computing for customer

--- lib/models/customer.rb
+++ lib/models/customer.rb
@@ -2,8 +2,8 @@ class Customer < Sequel::Model(:customers)
   unrestrict_primary_key
 
   one_to_many :wallets
-  one_to_many :payments, :order => :created_at
-  one_to_many :transactions
+  one_to_many :payments
+  one_to_many :transactions, :order => :created_at
 
   plugin :timestamps
 
